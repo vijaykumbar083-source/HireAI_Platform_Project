fa2916f1fd81757c0060f4cb5ed2b9470c3ac843
Vijayakumar <vijaykumbar083@gmail.com>
2026-08-04 10:04:17 +0530

    Addeded Recruiter Dashboard

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const userRouter=require('./routes/userRouter');
+const recruiterRouter=require('./routes/recruiterRouter');
 const app = express();
 
 app.set("view engine", "ejs");
@@ -10,6 +11,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use('/',userRouter);
 app.use('/user_dashboard',userRouter);
+app.use('/recruiter_dashboard',recruiterRouter);
 
 const PORT = 3000;
 app.listen(PORT, () => {
---
+++ controllers/recruiterController.js
@@ -0,0 +1,6 @@
+
+
+exports.recruiterdashboard=(req,res,next)=>{
+    console.log("Recruiter Dashboard");
+    res.render("recruiter-dashboard");
+}
