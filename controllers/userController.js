d0fb10b5731e22125ac4dc50f3bfbc7ee4968675
Vijayakumar <vijaykumbar083@gmail.com>
2026-08-05 20:28:44 +0530

    User Front End Done

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,33 @@
-
-exports.home=(req,res,next)=>{
-    console.log(req.url,req.body);
+exports.home = (req, res, next) => {
+    console.log(req.url, req.body);
     res.render("index");
-}
+};
 
-exports.user_dashboard=(req,res,next)=>{
+exports.user_dashboard = (req, res, next) => {
     console.log("User Dashboard");
     res.render("user-dashboard");
-}
+};
+
+exports.profile = (req, res, next) => {
+    res.render("profile");
+};
+
+exports.resume = (req, res, next) => {
+    res.render("resume");
+};
+
+exports.settings = (req, res, next) => {
+    res.render("user_settings");
+};
+
+exports.applications = (req, res, next) => {
+    res.render("applications");
+};
+
+exports.interviews = (req, res, next) => {
+    res.render("user_interview");
+};
+
+exports.jobs = (req, res, next) => {
+    res.render("jobs");
+};
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,9 +1,29 @@
-const express=require('express');
-const Controller=require('../controllers/userController')
-const router=express.Router();
+const express = require('express');
+const Controller = require('../controllers/userController');
+const router = express.Router();
 
+// Home Page
+router.get('/', Controller.home);
 
-router.get('/',Controller.home);
-router.get('/user_dashboard',Controller.user_dashboard);
+// User Dashboard
+router.get('/user_dashboard', Controller.user_dashboard);
 
-module.exports=router;
+// Profile
+router.get('/profile', Controller.profile);
+
+// Resume & ATS Analysis
+router.get('/resume', Controller.resume);
+
+// Settings
+router.get('/settings', Controller.settings);
+
+// Applications
+router.get('/applications', Controller.applications);
+
+// Interviews
+router.get('/interviews', Controller.interviews);
+
+// Jobs Listing
+router.get('/jobs', Controller.jobs);
+
+module.exports = router;
