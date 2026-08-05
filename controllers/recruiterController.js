3db79404f352bf8842c2f337f808c532f6c9b00f
Vijayakumar <vijaykumbar083@gmail.com>
2026-08-05 20:10:19 +0530

    FrontEnd For recruiter

--- controllers/recruiterController.js
+++ controllers/recruiterController.js
@@ -1,23 +1,29 @@
 exports.recruiterdashboard = (req, res) => {
+    console.log("recruiterdashboard");
     res.render("recruiter-dashboard");
 };
 
 exports.postjob = (req, res) => {
+    console.log("post job");
     res.render("post_job");
 };
 
 exports.managejobs = (req, res) => {
+    console.log("manage jobs");
     res.render("manage_jobs");
 };
 
 exports.viewapplicants = (req, res) => {
+    console.log("view applicants");
     res.render("view_applicants");
 };
 
 exports.interviewschedule = (req, res) => {
+    console.log("intervuew schedule");
     res.render("interview_schedule");
 };
 
 exports.companyprofile = (req, res) => {
+    console.log("company profile");
     res.render("company_profile");
 };
